ff696f96675b0417230d6e824f895d6c8c54ec86
[DAleksandar <[Daleksandarristic1993@gmail.com[>
2017-07-29 21:43:32 +0200

    R.Ž remont in progress

---
+++ php/email.php
@@ -0,0 +1,9 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "risticradovan60@gmail.com"; // this is your Email address
+    $email = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+?>
